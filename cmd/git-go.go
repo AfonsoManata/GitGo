c583427be7007ca91bcc79fbcb368288d8d85884
afonsomanata <afonso.manata.cruz@gmail.com>
2026-05-05 08:14:09 +0100

    To run with arguments you must build first

--- cmd/git-go.go
+++ cmd/git-go.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"cmp"
-	"compress/zlib"
-	"crypto/sha1"
-	"encoding/hex"
+	// "bufio"
+	// "bytes"
+	// "cmp"
+	// "compress/zlib"
+	// "crypto/sha1"
+	// "encoding/hex"
 	"fmt"
-	"io"
-	"net/http"
+	// "io"
+	// "net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"slices"
-	"strconv"
-	"strings"
-	"time"
+	// "path/filepath"
+	// "slices"
+	// "strconv"
+	// "strings"
+	// "time"
 	"log"
 )
 
 func main(){
 	// First element of os.Args is the path to the program and the others are the input
 
+	fmt.Println(os.Args)
+
 	if len(os.Args) < 2{
-		printUsageAndExit("")
+		fmt.Println("Invalid number of commands")
+		os.Exit(1)
 	}
 
 	switch os.Args[1]{
 	case "init":
 		gitInit()
-	case "add":
-		gitAdd()
 	default:
 		fmt.Println("Invalid Commands: %s, %s", os.Args[0], os.Args[1])
 		os.Exit(1)
@@ -49,7 +49,7 @@ func gitInit(){
 		
 		// Checks if the error is not that the file already exists
 		if err != nil && !os.IsExist(err){
-			log.fatal("error creating directory %s : %s", directory, err)
+			log.Fatal("error creating directory %s : %s", directory, err)
 		}
 
 	}
@@ -59,15 +59,15 @@ func gitInit(){
 	headContent := "ref: refs/heads/main"
 	err := os.WriteFile(headPath, []byte(headContent), 0644)
 	if err != nil{
-		log.fatal("Error while writing file %s : %s", headPath, err)
+		log.Fatal("Error while writing file %s : %s", headPath, err)
 	}
 
 	// Writing .git/description
 	descriptionPath := ".git/description"
 	descriptionContent := "Unnamed repository; edit this file 'description' to name the repository.\n"
-	err := os.WriteFile(descriptionPath, []byte(descriptionContent), 0644)
+	err = os.WriteFile(descriptionPath, []byte(descriptionContent), 0644)
 	if err != nil{
-		log.fatal("Error while writing file %s : %s", descriptionPath, err)
+		log.Fatal("Error while writing file %s : %s", descriptionPath, err)
 	}
 	
 	// Writing .git/config can be an upgrade
